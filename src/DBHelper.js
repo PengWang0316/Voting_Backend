8b6fa013a3f879bff1b708a0648506cc7376c90d
Kevin Wang <rocwang0316@gmail.com>
2018-10-29 17:26:06 -0700

    Added the DBHelper file

---
+++ src/DBHelper.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+require('dotenv').config(); // Loading .env to process.env
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+module.exports = {
+  getPool: () => pool,
+  getConnection: callback => pool.getConnection(callback),
+};
