91b65068a7675f22ab09a2d8f6417a949f650bbc
Kevin Wang <rocwang0316@gmail.com>
2018-11-02 14:57:52 -0700

    Added the users table schema

--- DBSchema.sql
+++ DBSchema.sql
@@ -6,3 +6,14 @@ CREATE TABLE `Kevin`.`candidates` (
   `isDemocray` TINYINT NOT NULL,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
+
+CREATE TABLE `Kevin`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `photo` VARCHAR(255) NULL,
+  `vote_id` INT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB;
